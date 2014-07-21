6e0ce733e82383b7146227fda041d70b6a7db3c3
Capster <m6277@yandex.ru>
2014-07-21 18:59:20 +0400

    Removed !Cake's code (Resize Grip)

--- lua/metro/ui/resizegrip.lua
+++ lua/metro/ui/resizegrip.lua
@@ -1,28 +1,17 @@
 local PANEL = {}
 
-function PANEL:Init ()
-	self:SetSize (24, 24)	
-	self:SetCursor ("sizenwse")
+function PANEL:Init()
+	self:SetSize(16, 16)
+	self:SetCursor("sizenwse")
 end
 
-function PANEL:Paint (w, h)
-	local padding = 3
-	local dotSize = math.min ((math.min (w, h) - padding * 2) / 5, 3)
-	dotSize = math.floor (dotSize + 0.5) -- round dotSize
-	
-	local x = w - padding - dotSize * 5
-	draw.RoundedBox (2, x, h - padding - dotSize, dotSize, dotSize, Metro.Colors.GripColor)
-	x = w - padding - dotSize * 3
-	draw.RoundedBox (2, x, h - padding - dotSize * 3, dotSize, dotSize, Metro.Colors.GripColor)
-	draw.RoundedBox (2, x, h - padding - dotSize, dotSize, dotSize, Metro.Colors.GripColor)
-	x = w - padding - dotSize
-	draw.RoundedBox (2, x, h - padding - dotSize * 5, dotSize, dotSize, Metro.Colors.GripColor)
-	draw.RoundedBox (2, x, h - padding - dotSize * 3, dotSize, dotSize, Metro.Colors.GripColor)
-	draw.RoundedBox (2, x, h - padding - dotSize, dotSize, dotSize, Metro.Colors.GripColor)
+function PANEL:Paint(w, h)
 end
 
-function PANEL:PerformLayout ()
-	self:SetPos (self:GetParent ():GetWide () - self:GetWide (), self:GetParent ():GetTall () - self:GetTall ())
+function PANEL:PerformLayout(w, h)
+	local parent = self:GetParent()
+	local _w, _h = parent:GetWide(), parent:GetTall()
+	self:SetPos(_w - w, _h - h)
 end
 
 Metro.Register("MetroResizeGrip", PANEL, "DPanel")
